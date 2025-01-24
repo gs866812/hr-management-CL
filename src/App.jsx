d2fb1692553f0454dea50cee9fb0c8b24670cb35
gs866812 <gooogle.sarwar@gmail.com>
2025-01-24 15:50:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-amber-400'>hello</div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import daisyui from 'daisyui';
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,jsx,ts,tsx}", // React files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [
+        daisyui,
+    ],
+  };
+  
